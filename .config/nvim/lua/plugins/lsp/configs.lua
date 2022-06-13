ccc67bbe8185a41e113b37d399f1171e5304a9aa
pietra <pietra.ferreira@embecosm.com>
2022-06-13 18:07:39 +0100

    added support to rust_analyzer lsp

--- .config/nvim/lua/plugins/lsp/configs.lua
+++ .config/nvim/lua/plugins/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "jdtls", "ccls" }
+local servers = { "jsonls", "sumneko_lua", "jdtls", "ccls", "rust_analyzer" }
 
 lsp_installer.setup {
 	ensure_installed = servers
